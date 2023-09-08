1495c2ce19b351ac09dcbdfa11d4f4a0dec69c09
makastn <mark.kastner@wilken.de>
2023-09-08 13:49:29 +0200

    removed comments from merge-tabbar.js

--- merge-tabbar.js
+++ merge-tabbar.js
@@ -1,36 +1,13 @@
-
 function addTabbarObserver(tabbar){
     const toolbar = document.querySelector('.mainbar .toolbar-mainbar:last-of-type');
     const config = {attributes: true};
-
-
-    // const mainbar = document.querySelector('.mainbar');
-    // const tabbar = document.querySelector('#tabs-tabbar-container');
-
-    // if(!mainbar || !tabbar) {
-    //     setTimeout(() => addTabbarObserver(), 300);
-    //     console.log('nope');
-    //     return;
-    // }
-
-    //const config = {
-      //  childList: true,
-        //subtree: true
-    //};
-
-
     const observer = new MutationObserver(moveTabbar.bind(this, tabbar));
     observer.observe(toolbar, config);
-
-    // urlBar = document.querySelector('.UrlBar-AddressField');
-    // tabs = document.querySelector('#tabs-tabbar-container');
-    // urlBar.after(tabs);
 }
 
 function moveTabbar(tabbar) {
     let visibleToolbar = document.querySelector('.toolbar-mainbar.toolbar-visible');
     let target = visibleToolbar.querySelector('.UrlBar-AddressField') ?? visibleToolbar.childNodes[visibleToolbar.childNodes.length - 1];
-    // let tabbar = document.querySelector('#tabs-tabbar-container');
 
     console.log('callback');
     target.after(tabbar);
