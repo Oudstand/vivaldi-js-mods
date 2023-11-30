c400047f704b951df42e56804006b6274bb3099b
makastn <mark.kastner@wilken.de>
2023-11-30 08:08:40 +0100

    code quality

--- unused/merge-tabbar.js
+++ unused/merge-tabbar.js
@@ -1,21 +1,21 @@
-function addTabbarObserver(tabbar){
-    const toolbar = document.querySelector('.mainbar .toolbar-mainbar:last-of-type');
-    const config = {attributes: true};
-    const observer = new MutationObserver(moveTabbar.bind(this, tabbar));
+function addTabbarObserver(tabbar) {
+    const toolbar = document.querySelector('.mainbar .toolbar-mainbar:last-of-type'),
+        config = {attributes: true},
+        observer = new MutationObserver(moveTabbar.bind(this, tabbar));
     observer.observe(toolbar, config);
 }
 
 function moveTabbar(tabbar) {
-    let visibleToolbar = document.querySelector('.toolbar-mainbar.toolbar-visible');
-    let target = visibleToolbar.querySelector('.UrlBar-AddressField') ?? visibleToolbar.childNodes[visibleToolbar.childNodes.length - 1];
+    let visibleToolbar = document.querySelector('.toolbar-mainbar.toolbar-visible'),
+        target = visibleToolbar.querySelector('.UrlBar-AddressField') ?? visibleToolbar.childNodes[visibleToolbar.childNodes.length - 1];
 
     console.log('callback');
     target.after(tabbar);
-};
+}
 
 setTimeout(function waitMergeTabbar() {
-    const browser = document.getElementById('browser');
-    const tabbar = document.querySelector('#tabs-tabbar-container');
+    const browser = document.getElementById('browser'),
+        tabbar = document.querySelector('#tabs-tabbar-container');
     if (browser) {
         moveTabbar(tabbar);
         addTabbarObserver(tabbar);
