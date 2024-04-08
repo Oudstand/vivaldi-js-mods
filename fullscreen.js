56332a58a66a92e3d010970e3a9b7b87b1ee3912
makastn <mark.kastner@wilken.de>
2024-04-08 10:37:11 +0200

    added support for hiding panels

--- fullscreen.js
+++ fullscreen.js
@@ -3,7 +3,10 @@
  * Hides the tab bar and address bar when not hovering
  */
 (function checkWebViewForFullscreen() {
-    const webView = document.querySelector("#webview-container");
+    const webView = document.querySelector("#webview-container")
+        hidePanels = true,
+        marginLeft = '0.5rem';
+
     if (!webView) {
         setTimeout(checkWebViewForFullscreen, 1337);
         return;
@@ -13,6 +16,7 @@
         browser = document.querySelector("#browser"),
         mainBar = document.querySelector(".mainbar"),
         bookmarkBar = document.querySelector(".bookmark-bar"),
+        panelsContainer = document.querySelector("#panels-container"),
         fullscreenEnabled;
 
     chrome.storage.local.get("fullScreenModEnabled").then((value) => {
@@ -35,6 +39,7 @@
             browser = document.querySelector("#browser");
             mainBar = document.querySelector(".mainbar");
             bookmarkBar = document.querySelector(".bookmark-bar");
+            panelsContainer = document.querySelector("#panels-container");
 
             setMarginAndZIndex(fullscreenEnabled);
             fullscreenEnabled ? hide() : show();
@@ -46,7 +51,12 @@
     );
 
     const style = document.createElement("style");
-    style.appendChild(document.createTextNode("[hidden] { transform: translateY(-100px) !important; }"));
+    style.appendChild(document.createTextNode(`
+        .fullscreen-listener-enabled #header, .fullscreen-listener-enabled .mainbar, .fullscreen-listener-enabled .bookmark-bar, .fullscreen-listener-enabled #panels-container { transition: transform .5s !important; }
+        [hidden] { transform: translateY(-100px) !important; }
+        .hidden-panel { transform: translateX(-100%); }
+        .fullscreen-listener-enabled #main { padding-top: 0 !important; }
+    `));
     document.head.appendChild(style);
 
     const hoverDiv = document.createElement("div");
@@ -56,7 +66,18 @@
     hoverDiv.style.left = "0";
     hoverDiv.style.top = "0";
     hoverDiv.style.zIndex = "1";
+    hoverDiv.pointerEvents = "none";
     document.body.insertBefore(hoverDiv, document.body.firstChild);
+
+    const panelHoverDiv = document.createElement("div");
+    if (hidePanels) {        
+        panelHoverDiv.style.height = "100%";
+        panelHoverDiv.style.width = "1rem";
+        panelHoverDiv.style.position = "fixed";
+        panelHoverDiv.style.left = "0";
+        panelHoverDiv.pointerEvents = "none";
+        panelsContainer.before(panelHoverDiv); 
+    }
 
     function toggleFullScreen() {
         fullscreenEnabled = !fullscreenEnabled;
@@ -66,14 +87,18 @@
     }
 
     function addFullScreenListener() {
+        document.querySelector("#app").classList.add("fullscreen-listener-enabled");
         webView.addEventListener("pointerenter", hide);
-        hoverDiv.addEventListener("pointerenter", show);
+        hoverDiv.addEventListener("pointerenter", showTop);
+        hidePanels && panelHoverDiv.addEventListener("pointerenter", showLeft);
         hide();
     }
 
     function removeFullScreenListener() {
+        document.querySelector("#app").classList.remove("fullscreen-listener-enabled");
         webView.removeEventListener("pointerenter", hide);
-        hoverDiv.removeEventListener("pointerenter", show);
+        hoverDiv.removeEventListener("pointerenter", showTop);
+        hidePanels && panelHoverDiv.removeEventListener("pointerenter", showLeft);
         show();
     }
 
@@ -81,15 +106,30 @@
         header.hidden = true;
         mainBar.hidden = true;
         bookmarkBar.hidden = true;
+       
+        if (hidePanels) {
+            panelsContainer.classList.add("hidden-panel");
+        }
     }
 
     function show() {
+        showTop();
+        showLeft();
+    }
+
+    function showTop() {
         header.hidden = false;
         mainBar.hidden = false;
         bookmarkBar.hidden = false;
 
         browser.classList.remove("address-top-off");
         browser.classList.add("address-top");
+    }
+
+    function showLeft() {
+        if (hidePanels) {
+            panelsContainer.classList.remove("hidden-panel");
+        }
     }
 
     function setMarginAndZIndex(shouldAdjustStyles) {
@@ -99,6 +139,10 @@
         adjustStyles(header, shouldAdjustStyles, 0);
         adjustStyles(mainBar, shouldAdjustStyles, headerOffset);
         adjustStyles(bookmarkBar, shouldAdjustStyles, headerOffset + mainBarOffset);
+
+        if (hidePanels) {
+            webView.style.marginLeft = shouldAdjustStyles ? `calc(-${panelsContainer.offsetWidth}px + ${marginLeft})` : "";
+        }
     }
 
     function adjustStyles(element, shouldAdjustStyles, offset) {
